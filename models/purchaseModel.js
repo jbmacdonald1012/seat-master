b70a07507ad1dcb291587bb5d452d52f4090150a
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2026-03-18 23:19:00 -0600

    Add initial scaffold for Purchase model with placeholder methods
    Signed-off-by: jbmacdonald1012 <jbmacdonald1012@gmail.com>

---
+++ models/purchaseModel.js
@@ -0,0 +1,21 @@
+import pool from '../db.js';
+
+export async function create(purchaseData) {
+  throw new Error('Not implemented');
+}
+
+export async function findById(id) {
+  throw new Error('Not implemented');
+}
+
+export async function findByUserId(userId) {
+  throw new Error('Not implemented');
+}
+
+export async function getAll() {
+  throw new Error('Not implemented');
+}
+
+export async function updateStatus(id, status) {
+  throw new Error('Not implemented');
+}
